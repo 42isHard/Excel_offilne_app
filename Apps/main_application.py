92cc730ec12282dd579ff157a97651beee4a6c2c
Francois Niagoloff <francoisniagoloff@live.fr>
2024-04-04 11:40:16 +0200

    Implementing Customtkinter

--- Apps/main_application.py
+++ Apps/main_application.py
@@ -1,32 +1,26 @@
-import tkinter as tk
-from tkinter import ttk
+import customtkinter as ctk
 from Apps.file_selection_page import FileSelectionPage
 
 
-class MainApplication(tk.Tk):
+class MainApplication(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Mon Application")
         self.geometry("600x450")
-        self.setup_styles()
-        self.main_frame = ttk.Frame(self, padding=10)
-        self.main_frame.pack(expand=True, fill='both')
+
+        ctk.set_appearance_mode("System")  # "System", "Dark", ou "Light"
+        ctk.set_default_color_theme("blue")
+
+        self.main_frame = ctk.CTkFrame(self)
+        self.main_frame.pack(expand=True, fill='both', padx=10, pady=10)  # Ajout de padding ici
         self.selected_filepaths = []
         self.create_main_menu()
 
-    def setup_styles(self):
-        style = ttk.Style(self)
-        style.theme_use('clam')
-        style.configure('TButton', background='#4a7a8c', foreground='white', font=('Arial', 10, 'bold'), borderwidth=1)
-        style.map('TButton', background=[('active', '#3b6978')])
-        style.configure('TLabel', background='#dae1e7', font=('Arial', 12, 'bold'))
-        style.configure('TFrame', background='#dae1e7')
-
     def create_main_menu(self):
         self.clear_frame()
-        ttk.Label(self.main_frame, text="Sélectionnez une fonctionnalité", style='TLabel').pack(pady=10)
-        ttk.Button(self.main_frame, text="Fonctionnalité de sélection de fichiers",
-                   command=self.open_file_selection).pack(pady=10)
+        ctk.CTkLabel(self.main_frame, text="Sélectionnez une fonctionnalité").pack(pady=10)
+        ctk.CTkButton(self.main_frame, text="Fonctionnalité de sélection de fichiers",
+                      command=self.open_file_selection).pack(pady=10)
 
     def open_file_selection(self):
         self.clear_frame()
@@ -39,8 +33,7 @@ class MainApplication(tk.Tk):
         for widget in self.main_frame.winfo_children():
             widget.destroy()
         self.main_frame.pack_forget()
-        self.main_frame.pack(expand=True, fill='both')
-
+        self.main_frame.pack(expand=True, fill='both', padx=10, pady=10)
 
 if __name__ == "__main__":
     app = MainApplication()
